17ad0593994aef998898d26b6cef81c2aa1f13e5
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2024-10-04 15:21:24 +0200

    feat: update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "homelab.day",
+  title: "Homelab - in one day",
   description: "Setup your homelab in one day.",
 };
 
--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -27,4 +27,3 @@ export function LandingPage() {
     </div>
   );
 }
-
